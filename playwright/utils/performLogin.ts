2c0f607d5d8ac6d644bce1a879d5ceb4b1f89ead
Drew Brokke <drew.brokke@liferay.com>
2024-10-08 13:23:36 -0500

    LPD-38104 SF

--- playwright/utils/performLogin.ts
+++ playwright/utils/performLogin.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Cookie, Page, expect} from '@playwright/test';
+import {Cookie, expect, Page} from '@playwright/test';
 
 const userData = {
 	'test': {
