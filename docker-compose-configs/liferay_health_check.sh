bcd73097f3eee90291a1032b3de1d27c0b95feae
Drew Brokke <drew.brokke@liferay.com>
2024-10-10 17:44:33 -0500

    LPD-38105 liferay-workspace-testing: WIP

--- docker-compose-configs/liferay_health_check.sh
+++
@@ -1,5 +0,0 @@
-test -d /opt/liferay/routes/default/liferay-sample-etc-node || exit 1
-
-test -d /opt/liferay/routes/default/liferay-sample-etc-spring-boot || exit 1
-
-curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/web/guest | grep 200 &>/dev/null
