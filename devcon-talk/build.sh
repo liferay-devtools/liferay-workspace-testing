d74b6efbd727b7d456bd7c052fc3c5b4d8c7d00f
Gregory Amerson <gregory.amerson@liferay.com>
2024-11-06 03:33:14 -0600

    beautysh

--- devcon-talk/build.sh
+++ devcon-talk/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for i in devcon*.markdown; do
-  echo "Processing markmap-cli $i"
-  npx markmap-cli --no-open $i
+    echo "Processing markmap-cli $i"
+    npx markmap-cli --no-open $i
 done
