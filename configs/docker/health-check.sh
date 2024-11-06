d3698c28cabc996c65538dab4588b1418c501297
Gregory Amerson <gregory.amerson@liferay.com>
2024-11-06 10:39:01 -0600

    move health check log to liferay logs folder

--- configs/docker/health-check.sh
+++ configs/docker/health-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function _log() {
-	echo "[$(date "+%Y.%m.%d %H:%M:%S")] $1" >> health-check.log
+	echo "[$(date "+%Y.%m.%d %H:%M:%S")] $1" >> logs/health-check.log
 }
 
 _log "Checking for license registration..."
